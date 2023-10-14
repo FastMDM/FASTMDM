c9744e616b18da6e533b58fd131f934c9d3ddb13
«Sergey <svronskiy@outlook.com>
2023-10-14 09:23:14 +0700

    +coaching

---
+++ backend/src/src/api/coaching/controllers/coaching.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  coaching controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::coaching.coaching');
