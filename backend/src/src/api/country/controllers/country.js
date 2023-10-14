665c173e92cefaf49f3924b8a16f20de11e56e32
«Sergey <svronskiy@outlook.com>
2023-10-14 09:23:38 +0700

    +country

---
+++ backend/src/src/api/country/controllers/country.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  country controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::country.country');
---
+++ backend/src/src/api/country/services/country.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * country service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::country.country');
