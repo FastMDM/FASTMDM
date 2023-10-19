6d0d621e30be0264ce945b149a4f13f66f6dbc5f
«Sergey <svronskiy@outlook.com>
2023-10-19 23:29:05 +0700

    +restaurant-cuisine

---
+++ backend/src/src/api/restaurant-cuisine/controllers/restaurant-cuisine.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * restaurant-cuisine controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::restaurant-cuisine.restaurant-cuisine');
