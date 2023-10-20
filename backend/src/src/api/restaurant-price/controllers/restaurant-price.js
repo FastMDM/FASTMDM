27856b27cea7dc7c785df019a472754c6ec6c847
«Sergey <svronskiy@outlook.com>
2023-10-20 18:37:28 +0700

    +restaurant-price

---
+++ backend/src/src/api/restaurant-price/controllers/restaurant-price.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * restaurant-price controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::restaurant-price.restaurant-price');
