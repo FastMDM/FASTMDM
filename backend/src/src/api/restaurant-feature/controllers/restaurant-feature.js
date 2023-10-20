36857beb3cbd57d3c81c3e79ba466352fa22fbaa
«Sergey <svronskiy@outlook.com>
2023-10-20 18:36:15 +0700

    +restaurant-feature

---
+++ backend/src/src/api/restaurant-feature/controllers/restaurant-feature.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * restaurant-feature controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::restaurant-feature.restaurant-feature');
