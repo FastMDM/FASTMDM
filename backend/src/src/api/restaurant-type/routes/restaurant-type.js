b92830f98b9ea9db80b03b7b5ccc5e7b8aaa5873
«Sergey <svronskiy@outlook.com>
2023-10-21 19:54:24 +0700

    +restaurant-type

---
+++ backend/src/src/api/restaurant-type/routes/restaurant-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * restaurant-type router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::restaurant-type.restaurant-type');
