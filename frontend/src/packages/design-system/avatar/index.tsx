ca187762aa95fb590b35af39db1a74e0a84fd743
«Sergey <svronskiy@outlook.com>
2023-11-14 22:03:10 +0700

    +avatar

---
+++ frontend/src/packages/design-system/avatar/index.tsx
@@ -0,0 +1,56 @@
+import { ReactNode, useMemo } from "react";
+
+import { Image } from "@showtime-xyz/universal.image";
+import type { TW } from "@showtime-xyz/universal.tailwind";
+import { View } from "@showtime-xyz/universal.view";
+
+import { CONTAINER_TW, IMAGE_TW, DEFAULT_AVATAR_PIC } from "./constants";
+
+export type AvatarProps = {
+  url?: string;
+  size?: number;
+  borderRadius?: number;
+  tw?: TW;
+  children?: ReactNode;
+  alt: string;
+};
+
+const getAvatarImageUrl = (imgUrl: string, size: number) => {
+  if (imgUrl && imgUrl.includes("https://lh3.googleusercontent.com")) {
+    imgUrl = imgUrl.split("=")[0] + "=s" + size * 2;
+  }
+  return imgUrl;
+};
+
+export const Avatar = ({
+  url,
+  borderRadius = 0,
+  size = 32,
+  tw = "",
+  children,
+  alt = "",
+}: AvatarProps) => {
+  const imageSource = useMemo(
+    () => ({ uri: getAvatarImageUrl(url || DEFAULT_AVATAR_PIC, size) }),
+    [url, size]
+  );
+
+  return (
+    <View
+      tw={[CONTAINER_TW, Array.isArray(tw) ? tw.join(" ") : tw]}
+      style={{ height: size, width: size }}
+    >
+      <Image
+        source={imageSource}
+        width={size}
+        height={size}
+        borderRadius={borderRadius}
+        resizeMode="cover"
+        tw={IMAGE_TW}
+        style={{ height: size, width: size, borderRadius: borderRadius }}
+        alt={alt}
+      />
+      {children}
+    </View>
+  );
+};
