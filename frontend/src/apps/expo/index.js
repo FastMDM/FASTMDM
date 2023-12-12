fdff027566af6baed3435f86147f069e16c8499f
«Sergey <svronskiy@outlook.com>
2023-12-12 22:09:53 +0700

    fix-3

--- frontend/src/apps/expo/index.js
+++ frontend/src/apps/expo/index.js
@@ -5,4 +5,4 @@ import App from './App'
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App)
+registerRootComponent(App);
