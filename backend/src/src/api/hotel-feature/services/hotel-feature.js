c82d25749a908777caed6d7bee389cdbac4d2af1
«Sergey <svronskiy@outlook.com>
2024-01-17 23:12:49 +0700

    update userGroup

---
+++ backend/src/src/api/hotel-feature/services/hotel-feature.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * hotel-feature service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::hotel-feature.hotel-feature');
---
+++ backend/src/src/api/hotel/routes/hotel.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * hotel router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::hotel.hotel');
