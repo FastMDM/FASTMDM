2f34aeb57179a0c0c5b78b4d7ea1890c452ebd2e
«Sergey <svronskiy@outlook.com>
2024-05-01 23:08:13 +0700

    -image

--- frontend/src/packages/app/components/Dropdowns/UserDropdown.js
+++ frontend/src/packages/app/components/Dropdowns/UserDropdown.js
@@ -28,11 +28,13 @@ const UserDropdown = () => {
       >
         <div className="items-center flex">
           <span className="w-12 h-12 text-sm text-white bg-blueGray-200 inline-flex items-center justify-center rounded-full">
+          /*
             <img
               alt="..."
               className="w-full rounded-full align-middle border-none shadow-lg"
               src="/img/team-1-800x800.jpg"
             />
+            */
           </span>
         </div>
       </a>
