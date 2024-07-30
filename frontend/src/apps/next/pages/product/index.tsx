77dfda0dba3ff3fb81f78280be22b4cbf87c491b
«Sergey <svronskiy@outlook.com>
2024-07-30 11:12:44 +0700

    product fix 01

--- frontend/src/apps/next/pages/product/index.tsx
+++ frontend/src/apps/next/pages/product/index.tsx
@@ -1,3 +1,22 @@
 import { ProductScreen } from 'app/screens/product'
+import type {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+} from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 export default ProductScreen
+
+type Props = {
+  // Add custom props here
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async ({
+  locale,
+}) => ({
+  props: {
+    ...(await serverSideTranslations(locale ?? 'en', ['common' ])),
+  },
+})
+
+
