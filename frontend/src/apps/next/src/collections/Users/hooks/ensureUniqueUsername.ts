299aa4ee75d970a76f0a030aa61b776ad162c9a4
«Sergey <svronskiy@outlook.com>
2025-02-03 22:48:24 +0700

    fix 01

--- frontend/src/apps/next/src/collections/Users/hooks/ensureUniqueUsername.ts
+++ frontend/src/apps/next/src/collections/Users/hooks/ensureUniqueUsername.ts
@@ -1,27 +1,36 @@
-import type { FieldHook } from 'payload'
+import type { FieldHook } from "payload"
+import { ValidationError } from "payload";
+import type { User } from "@/payload-types";
 
-import { ValidationError } from 'payload'
+import { getUserTenantIDs } from "../../../utilities/getUserTenantIDs";
 
-import { getUserTenantIDs } from '../../../utilities/getUserTenantIDs'
-
-export const ensureUniqueUsername: FieldHook = async ({ data, originalDoc, req, value }) => {
+export const ensureUniqueUsername: FieldHook<User> = async ({
+  data,
+  originalDoc,
+  req,
+  value,
+}) => {
   // if value is unchanged, skip validation
-  if (originalDoc.username === value) {
-    return value
+  if (originalDoc?.username === value) {
+    return value;
   }
 
-  const incomingTenantID = typeof data?.tenant === 'object' ? data.tenant.id : data?.tenant
-  const currentTenantID =
-    typeof originalDoc?.tenant === 'object' ? originalDoc.tenant.id : originalDoc?.tenant
-  const tenantIDToMatch = incomingTenantID || currentTenantID
+  const incomingTenantIds =
+    data?.tenants?.map((entry) => entry.id as any as number).join(",") || "";
+  const currentTenantIds =
+    originalDoc?.tenants?.map((entry) => entry.id as any as number).join(",") ||
+    "";
+
+  const tenantIDToMatch =
+    incomingTenantIds.length > 0 ? incomingTenantIds : currentTenantIds;
 
   const findDuplicateUsers = await req.payload.find({
-    collection: 'users',
+    collection: "users",
     where: {
       and: [
         {
-          'tenants.tenant': {
-            equals: tenantIDToMatch,
+          "tenants.tenant": {
+            in: tenantIDToMatch,
           },
         },
         {
@@ -31,37 +40,37 @@ export const ensureUniqueUsername: FieldHook = async ({ data, originalDoc, req, 
         },
       ],
     },
-  })
+  });
 
   if (findDuplicateUsers.docs.length > 0 && req.user) {
-    const tenantIDs = getUserTenantIDs(req.user)
+    const tenantIDs = getUserTenantIDs(req.user);
     // if the user is an admin or has access to more than 1 tenant
     // provide a more specific error message
-    if (req.user.roles?.includes('super-admin') || tenantIDs.length > 1) {
+    if (req.user.roles?.includes("super-admin") || tenantIDs.length > 1) {
       const attemptedTenantChange = await req.payload.findByID({
         id: tenantIDToMatch,
-        collection: 'tenants',
-      })
+        collection: "tenants",
+      });
 
       throw new ValidationError({
         errors: [
           {
             message: `The "${attemptedTenantChange.name}" tenant already has a user with the username "${value}". Usernames must be unique per tenant.`,
-            path: 'username',
+            path: "username",
           },
         ],
-      })
+      });
     }
 
     throw new ValidationError({
       errors: [
         {
           message: `A user with the username ${value} already exists. Usernames must be unique per tenant.`,
-          path: 'username',
+          path: "username",
         },
       ],
-    })
+    });
   }
 
-  return value
-}
+  return value;
+};
